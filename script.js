4b5ce121c8287536366ca9a8eb970f3cad358bcc
Nachiket <109031731+nachiket04@users.noreply.github.com>
2025-02-08 20:31:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
 	if (userInput === secretCode) {
-		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
 		resultDiv.innerHTML = 'Who the hell are you only Pooja can Login here!!!';
